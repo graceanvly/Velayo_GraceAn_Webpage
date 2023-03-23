7daec868b01270695fb613ee95710526adcc016d
Grace An Velayo <graceann151@gmail.com>
2023-03-23 20:14:34 +0800

    First Commit

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+    return (
+        <center>
+            <div style={{
+                backgroundImage: `url(${process.env.PUBLIC_URL + 'https://wallpapers.com/images/hd/homely-light-green-plain-bs9yf7py3z72163f.jpg'})`
+            }}>
+                <br></br>
+                <br></br>
+                <br></br>
+                <h1>AGRI SHOP</h1>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <form>
+                    <label>
+                        Your Name: 
+                        <li><input type="text" name="name" /></li>
+                        Email Address:
+                        <li><input type="text" name="email" /></li>
+                        Password
+                        <li><input type="text" name="password" /></li>
+                    </label>
+                    <li><input type="submit" value="Login" /></li> 
+                </form>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+            </div>
+        </center>
+        
+  );
+}
+
+
+export default App;
